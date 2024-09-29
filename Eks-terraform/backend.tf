4602a784757b07e73537edb4f84446ce6cd3fce1
Hein Htet Aung <heinhtet@OpenShellMM-2.local>
2024-09-29 22:32:54 +0630

    Updated docker-compose

--- Eks-terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "mrcloudbooktwotier" # Replace with your actual S3 bucket name
-    key    = "EKS-Ajay/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
